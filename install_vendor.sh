515f06cc8f304ae08f8315cf3a1617fe2c00fdc4
Hunter Blanks <hblanks@artifex.org>
2022-04-01 08:42:31 -0700

    add start of 2022 talk w/newer reveal.js

--- install_vendor.sh
+++ install_vendor.sh
@@ -1,12 +1,15 @@
 #!/bin/sh -e
 
-DEST_DIR=$1
+DEST_DIR=$(cd $(dirname $0)/vendor; pwd)
 
-# mkdir -p $DEST_DIR/reveal.js
-# curl -L -o $DEST_DIR/reveal.js.tar.gz https://github.com/hakimel/reveal.js/archive/3.6.0.tar.gz
-# tar --strip-components=1 -C $DEST_DIR/reveal.js \
-#     -xzf $DEST_DIR/reveal.js.tar.gz
-# rm $DEST_DIR/reveal.js.tar.gz
+REVEAL_VERSION=4.3.1
+REVEAL_DIR=$DEST_DIR/reveal.js-$REVEAL_VERSION
+
+mkdir -p $REVEAL_DIR
+curl -L -o /tmp/reveal.js.tar.gz https://github.com/hakimel/reveal.js/archive/refs/tags/4.3.1.tar.gz
+tar --strip-components=1 -C $REVEAL_DIR \
+    -xzf /tmp/reveal.js.tar.gz
+rm /tmp/reveal.js.tar.gz
 
 # mkdir -p $DEST_DIR/MathJax
 # curl -L -o $DEST_DIR/MathJax.tar.gz https://github.com/mathjax/MathJax/archive/2.7.4.tar.gz
@@ -14,6 +17,6 @@ DEST_DIR=$1
 #     -xzf $DEST_DIR/MathJax.tar.gz
 # rm $DEST_DIR/MathJax.tar.gz
 
-mkdir -p $DEST_DIR/highlight.js
-curl -L -o $DEST_DIR/highlight.js/default.min.css \
-    https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/default.min.css
+#mkdir -p $DEST_DIR/highlight.js
+#curl -L -o $DEST_DIR/highlight.js/default.min.css \
+#    https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/default.min.css
