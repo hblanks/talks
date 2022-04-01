22b3390fa794059e30c936fe155edb4714b0a649
Hunter Blanks <hblanks@artifex.org>
2022-04-01 09:12:27 -0700

    add 2019 talk, update reveal...somehow

--- install_vendor.sh
+++ install_vendor.sh
@@ -2,18 +2,24 @@
 
 DEST_DIR=$1
 
-# mkdir -p $DEST_DIR/reveal.js
-# curl -L -o $DEST_DIR/reveal.js.tar.gz https://github.com/hakimel/reveal.js/archive/3.6.0.tar.gz
-# tar --strip-components=1 -C $DEST_DIR/reveal.js \
-#     -xzf $DEST_DIR/reveal.js.tar.gz
-# rm $DEST_DIR/reveal.js.tar.gz
+if [ -z "$DEST_DIR" ]; then
+    echo "Usage: $0 DEST_DIR" >&2
+    exit 1
+fi
 
-# mkdir -p $DEST_DIR/MathJax
-# curl -L -o $DEST_DIR/MathJax.tar.gz https://github.com/mathjax/MathJax/archive/2.7.4.tar.gz
-# tar --strip-components=1 -C $DEST_DIR/MathJax \
-#     -xzf $DEST_DIR/MathJax.tar.gz
-# rm $DEST_DIR/MathJax.tar.gz
+
+mkdir -p $DEST_DIR/reveal.js
+curl -L -o $DEST_DIR/reveal.js.tar.gz https://github.com/hakimel/reveal.js/archive/3.7.0.tar.gz
+tar --strip-components=1 -C $DEST_DIR/reveal.js \
+    -xzf $DEST_DIR/reveal.js.tar.gz
+rm $DEST_DIR/reveal.js.tar.gz
+
+mkdir -p $DEST_DIR/MathJax
+curl -L -o $DEST_DIR/MathJax.tar.gz https://github.com/mathjax/MathJax/archive/2.7.5.tar.gz
+tar --strip-components=1 -C $DEST_DIR/MathJax \
+    -xzf $DEST_DIR/MathJax.tar.gz
+rm $DEST_DIR/MathJax.tar.gz
 
 mkdir -p $DEST_DIR/highlight.js
 curl -L -o $DEST_DIR/highlight.js/default.min.css \
-    https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/default.min.css
+    https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.13.1/styles/default.min.css
--- package_talk.sh
+++ package_talk.sh
@@ -2,4 +2,4 @@
 
 DIRNAME=$(dirname $1)
 BASENAME=$(basename $1)
-tar -C $DIRNAME --exclude=.git -h -czf - $BASENAME
+tar -h -C $DIRNAME --exclude=.git -h -czf - $BASENAME
